7fc78125833f10bfdfb0c890c985b2fb29577704
Syed Khizar <syed@ketto.org>
2023-01-24 19:43:00 +0530

    update: added fetched image and blog

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,12 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Posts({posts}: any) {
   return (
-    <div className='flex items-center'>
+    <div className='flex items-center space-x-5 justify-center mt-5'>
         {posts.map((post: any) => {
-          return <div className='p-5'> 
-            <img src="" alt="" />
+          return <Link href={`/posts/${post?.attributes?.slug}`} key={post.id}>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg cursor-pointer">
+            <img className='object-contain w-full' src={`http://localhost:1337${post?.attributes?.blog_media?.data?.attributes.url}`} alt="" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{post?.attributes?.title}</div>
+              <p className="text-gray-700 text-xs">
+                {post?.attributes?.blog_description}
+              </p>
+            </div>
           </div>
+          </Link>
         })}
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
 import { API_TOKENS } from '../api'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import Posts from '../components/Posts'
 
-const Home: NextPage = () => {
-  const [posts, setPosts] = useState({});
-  useEffect(() => {
-    (async () => {
-      const res = await fetch('http://localhost:1337/api/blogs-plural', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ${API_TOKENS.BACKEND_TOKEN}`
-        }
-      });
-      const postsRes = await res.json();
-      setPosts(postsRes.data);
-    })();
-  },[])
+const Home: NextPage = ({posts}: any) => {
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
@@ -33,5 +18,24 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:1337/api/blogs-plural?populate=*`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${API_TOKENS.BACKEND_TOKEN}`
+    }
+  });
+  const postsRes = await res.json();
+  console.log(postsRes.data[0].attributes.blog_media.data.attributes)
+  return {
+    props: {
+      posts: postsRes.data
+    }
+  }
+}
+
 
 export default Home
